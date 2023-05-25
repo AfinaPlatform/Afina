67c8670f8115dea4df1af0861e01067ea9be9bbf
Afina <afina.platform@gmail.com>
2023-05-26 00:38:25 +0600

    On branch master 	Add e2e tests

--- apps/promo/src/bundles/index/index.tsx
+++ apps/promo/src/bundles/index/index.tsx
@@ -7,10 +7,12 @@ export default function Index() {
         <h1 className={css.header}>Afina</h1>
       </div>
       <div className={css.welcomeWrapper}>
-        <p className={css.welcome}>Welcome to our online learning platform!</p>
+        <p data-welcome-text className={css.welcome}>
+          Welcome to our online learning platform!
+        </p>
       </div>
       <div className={css.textWrapper}>
-        <p className={css.text}>
+        <p data-info-text className={css.text}>
           We believe that learning should be an engaging and enjoyable
           experience, which is why we&apos;ve designed our platform to be as fun
           and interesting as watching a movie. With our engaging multimedia
@@ -19,7 +21,9 @@ export default function Index() {
           enjoyable than ever before :)
         </p>
       </div>
-      <p className={css.soon}>Soon...</p>
+      <p data-button-stub className={css.soon}>
+        Soon...
+      </p>
     </div>
   );
 }
